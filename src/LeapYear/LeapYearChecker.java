d961dfd1c49b7f5aaf9313721ac5ff20d1b84817
Jun Jeremy <jjtabanag@gmail.com>
2022-12-16 13:12:47 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearChecker.java
+++ src/LeapYear/LeapYearChecker.java
@@ -1,9 +1,7 @@
 package LeapYear;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 public class LeapYearChecker extends JFrame {
     private JPanel jpanel;
@@ -11,10 +9,31 @@ public class LeapYearChecker extends JFrame {
     private JButton btnCheckYear;
 
     public LeapYearChecker() {
-        btnCheckYear.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int year = Integer.parseInt(tfYear.getText());
+        btnCheckYear.addActionListener(new CheckButton());
+        tfYear.addKeyListener(new CheckButton());
+    }
+
+    public static void main(String[] args) {
+        LeapYearChecker app = new LeapYearChecker();
+        app.setContentPane(app.jpanel);
+        app.setSize(500,500);
+        app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
+
+    public class CheckButton implements ActionListener, KeyListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                String str = tfYear.getText();
+                char[] c = str.toCharArray();
+                for (int i = 0; i < str.length(); i++) {
+                    if (!Character.isDigit(c[i])) {
+                        throw new NumberFormatException("Invalid Input");
+                    }
+                }
+                int year = Integer.parseInt(str);
                 Boolean leap;
                 if (year % 4 == 0) {
                     if (year % 100 == 0) {
@@ -31,20 +50,66 @@ public class LeapYearChecker extends JFrame {
 
                 if (leap) {
                     JOptionPane.showMessageDialog(null, "Leap Year");
+                    System.out.println(year + " is a Leap Year");
                 } else {
                     JOptionPane.showMessageDialog(null, "Not a Leap Year");
+                    System.out.println(year + " is not a Leap Year");
                 }
-
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+                System.out.println(ex.getMessage());
             }
-        });
-    }
+        }
 
-    public static void main(String[] args) {
-        LeapYearChecker app = new LeapYearChecker();
-        app.setContentPane(app.jpanel);
-        app.setSize(500,500);
-        app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        app.setVisible(true);
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                try {
+                    String str = tfYear.getText();
+                    char[] c = str.toCharArray();
+                    for (int i = 0; i < str.length(); i++) {
+                        if (!Character.isDigit(c[i])) {
+                            throw new NumberFormatException("Invalid Input");
+                        }
+                    }
+                    int year = Integer.parseInt(str);
+                    Boolean leap;
+                    if (year % 4 == 0) {
+                        if (year % 100 == 0) {
+                            if (year % 400 == 0)
+                                leap = true;
+                            else
+                                leap = false;
+                        }
+                        else
+                            leap = true;
+                    }
+                    else
+                        leap = false;
+
+                    if (leap) {
+                        JOptionPane.showMessageDialog(null, "Leap Year");
+                        System.out.println(year + " is a Leap Year");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Not a Leap Year");
+                        System.out.println(year + " is not a Leap Year");
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
     }
 }
 
