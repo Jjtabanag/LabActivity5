7b49c96383d412210acf6d70fee2a92ee9919c3f
Jun Jeremy <jjtabanag@gmail.com>
2022-12-16 10:25:51 +0800

    Add exception handling to FoodOrdering

--- src/FoodOrdering/FoodOrderingSystem.java
+++ src/FoodOrdering/FoodOrderingSystem.java
@@ -35,38 +35,48 @@ public class FoodOrderingSystem extends JFrame {
                 foods.add(cTea);
                 foods.add(cSundae);
 
-                int ctr = 0;
-                for (JCheckBox cb : foods) {
-                    if (cb.isSelected()) {
-                        double price = Double.parseDouble(cb.getName());
-                        total += price;
-                        ctr++;
+                try {
+                    int ctr = 0;
+                    for (JCheckBox cb : foods) {
+                        if (cb.isSelected()) {
+                            double price = Double.parseDouble(cb.getName());
+                            total += price;
+                            ctr++;
+                        }
                     }
-                }
-
-                double discount = 0;
-                if (rb5.isSelected()) {
-                    discount = total * .05;
-                    total -= discount;
-                } else if (rb10.isSelected()) {
-                    discount = total * .1;
-                    total -= discount;
-                } else if (rb15.isSelected()) {
-                    discount = total * .15;
-                    total -= discount;
-                } else if (rbNone.isSelected()) {
-                    discount = 0;
-                }
-
-                for (JCheckBox cb : foods) {
-                    if (cb.isSelected()) {
-                        double price = Double.parseDouble(cb.getName());
-                        System.out.println(cb.getText() + ": Php " + df.format(price));
+                    if (ctr == 0) {
+                        throw new Exception("No food option selected.");
                     }
+                    double discount = 0;
+                    if (rb5.isSelected()) {
+                        discount = total * .05;
+                    } else if (rb10.isSelected()) {
+                        discount = total * .1;
+                    } else if (rb15.isSelected()) {
+                        discount = total * .15;
+                    } else if (rbNone.isSelected()) {
+                        discount = 0;
+                    } else {
+                        throw new Exception("No discount option selected.");
+                    }
+
+                    System.out.println("RECEIPT:");
+                    for (JCheckBox cb : foods) {
+                        if (cb.isSelected()) {
+                            double price = Double.parseDouble(cb.getName());
+                            System.out.println(cb.getText() + ": Php " + df.format(price));
+                        }
+                    }
+
+                    System.out.println("Sub Total: Php " + df.format(total));
+                    System.out.println("Discount: Php " + df.format(discount));
+                    System.out.println("Total: Php " + df.format(total - discount) + "\n");
+                    JOptionPane.showMessageDialog(null, "The total price is Php " + df.format(total));
+
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                    System.out.println(ex.getMessage());
                 }
-                System.out.println("Discount: Php " + df.format(discount));
-                System.out.println("Total: Php " + df.format(total) + "\n");
-                JOptionPane.showMessageDialog(null, "The total price is Php " + df.format(total));
             }
         });
     }
